77e67c008613ea0c3d226ec1fad95502c5b93293
y_kisse <yudaikichise@gmail.com>
2020-07-09 20:20:44 +0900

    [update] return error response when authorization header is invalid.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
-import { ApolloServer } from "apollo-server-cloud-functions";
+import { ApolloServer, AuthenticationError } from "apollo-server-cloud-functions";
 import { readFileSync } from 'fs';
 import * as path from 'path';
 import { resolvers } from "./resolvers/index";
@@ -12,12 +12,27 @@ const server = new ApolloServer({
   ,resolvers
   ,context: async ({ req, res }) => {
     const firebaseAuthToken = req.headers.authorization;
+    // Authorization ヘッダが存在しない場合, もしくは空白の場合にはエラーを返す.
+    if (!firebaseAuthToken) {
+      throw new AuthenticationError("Authorization header is not found.");
+    }
+
+    if (firebaseAuthToken == "") {
+      throw new AuthenticationError("Authorization header must not be blank");
+    }
+
     const currentUID = await admin.auth().verifyIdToken(firebaseAuthToken)
       .then(decodedToken => {
         return decodedToken.uid;
       }).catch(err => {
         console.error(err);
       });
+
+    // currentUID が null の場合には, 正しくないトークンが Authorization に渡されたのでエラーを返す.
+    if (!currentUID) {
+      throw new AuthenticationError("Authorization faild. Check id token.");
+    }
+
     return { currentUID };
   }
   ,introspection: true
